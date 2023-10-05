0ecb5487f9a9329f5225f1e4aba1ad7aaccec3e4
Manish Kumar Akela <82595665+manish-gkv@users.noreply.github.com>
2023-10-06 02:52:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 
 // TASK_1: FUNCTION_TO_ENCRYPT_THE_TEXT
 
-function EncryptText(text, n) {
+function EncryptText() {
     // text -> THE_TEXT_THAT_TO_ENCRYPT
+    let text=document.getElementById("message").value;
+    let n=document.getElementById("key").value;
+    n=parseInt(n)
     // n -> NUMBER_OF_SHIFT
     
     let ans = "";
@@ -13,31 +16,33 @@ function EncryptText(text, n) {
 
     // iterate over the given text
     for (let i = 0; i < text.length; i++) {
-        const ch = text.charAt(i);
+        let ch = text.charAt(i);
         // check if space is there then simply add space
         if (ch === " ") {
             ans += " ";
         } else if (ch === ch.toUpperCase()) {
             // check if a character is uppercase then encrypt it accordingly
-            const e = (ch.charCodeAt(0) + n - 65) % 26 + 65;
+            let e = (ch.charCodeAt(0) + n - 65) % 26 + 65;
             ans += String.fromCharCode(e);
         } else {
             // check if a character is lowercase then encrypt it accordingly
-            const e = (ch.charCodeAt(0) + n - 97) % 26 + 97;
+            let e = (ch.charCodeAt(0) + n - 97) % 26 + 97;
             ans += String.fromCharCode(e);
         }
     }
     
     // return the encrypted text
-    return ans;
+    document.getElementById("encrpted-text").value=ans;
 }
 
 // TASK_2: FUNCTION_TO_DECRYPT_THE_TEXT
 
-function DecryptText(ciphertext, n) {
+function DecryptText() {
     // ciphertext -> THE_TEXT_THAT_TO_DECRYPT
+    let ciphertext=document.getElementById("cipher-text").value
     // n -> NUMBER_OF_SHIFT
-    
+    let n=parseInt(document.getElementById("ci-key").value)
+
     let ans = "";
     // Decrypted_Text_STORES_IN_ans_VARIABLE
 
@@ -59,19 +64,28 @@ function DecryptText(ciphertext, n) {
     }
 
     // return the decrypted text
-    return ans;
+    document.getElementById("decrpted-text").value=ans
 }
 
 // TASK_3: Guessing_The_Key:
 
 // Part_1
-function KeyFinder(ciphertext, plaintext) {
+function KeyFinder() {
+    let ciphertext=document.getElementById("ci-text").value
+    let plaintext=document.getElementById("org-text").value
     // if a positive number is returned, it means RIGHT_Shift, else LEFT_Shift
-    return ciphertext.charCodeAt(0) - plaintext.charCodeAt(0);
+    let key= ciphertext.charCodeAt(0) - plaintext.charCodeAt(0);
+    if(key<0){
+        document.getElementById("f-key").value=((-1)*key)+" Right Shift"
+    }
+    else{
+        document.getElementById("f-key").value=key+" Left Shift"
+    }
 }
 
 // Part_2
-function bruteForceCC(message) {
+function bruteForceCC() {
+    let message=document.getElementById("msg").value;
     const Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     // loop to run all 26 keys from 0 to 25
@@ -93,8 +107,10 @@ function bruteForceCC(message) {
                 translated += ch;
             }
         }
-
-        console.log(`Hacking key is ${key}: ${translated}`);
+        let res=`Hacking key is ${key}: ${translated}`;
+        //console.log(`Hacking key is ${key}: ${translated}`);
+        let idd = `sp-${key+1}`;
+        document.getElementById(idd).innerHTML=res;
     }
 }
 
